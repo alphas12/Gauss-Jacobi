f8d49697df5cadabaab6d3f3180919f0cfa9d8fd
Sharly Pia Rose Barago <155881364+sharly-barago@users.noreply.github.com>
2026-05-07 15:13:39 +0800

    Definitions Page - GS & Jacobi
    + Added Definitions Pages for GS & Jacobi

--- FINALACTIVITY_GROUP2.R
+++ FINALACTIVITY_GROUP2.R
@@ -1,5 +1,21 @@
 library(shiny)
 
+uses_gs <- list(
+  list("Vestibulum feugiat dui quis diam convallis mattis."),
+  list("Integer vehicula diam sed ligula ", span("highlighted text", class = "highlighted-text"), " commodo fringilla."),
+  list("Lorem ipsum dolor sit amet, ", span("highlighted text", class = "highlighted-text"), " adipiscing elit."),
+  list("Vestibulum feugiat dui quis diam convallis mattis."),
+  list("Integer vehicula diam sed ligula ", span("highlighted text", class = "highlighted-text"), " commodo fringilla.")
+)
+
+uses_jacobi <- list(
+  list("Vestibulum feugiat dui quis diam convallis mattis."),
+  list("Integer vehicula diam sed ligula ", span("highlighted text", class = "highlighted-text"), " commodo fringilla."),
+  list("Lorem ipsum dolor sit amet, ", span("highlighted text", class = "highlighted-text"), " adipiscing elit."),
+  list("Vestibulum feugiat dui quis diam convallis mattis."),
+  list("Integer vehicula diam sed ligula ", span("highlighted text", class = "highlighted-text"), " commodo fringilla.")
+)
+
 ui <- fluidPage(
   
   tags$head(
@@ -41,12 +57,13 @@ ui <- fluidPage(
       
       .body-text {
         color: #164670;
-        text-align: center;
+        text-align: justify;
         font-family: IBM-Plex-Sans;
         font-size: 20px;
         font-style: normal;
         font-weight: 400;
         line-height: normal;
+        margin: 0;
       }
       
 
@@ -113,7 +130,8 @@ ui <- fluidPage(
         justify-content: space-evenly;
         flex-direction: row;
         align-items: center;
-        min-height: 100vh;  
+        min-height: 100vh;
+        max-height: 100vh;
       }
   
       .landing-right{
@@ -123,8 +141,90 @@ ui <- fluidPage(
         width: 30%;
       }
       
-      .landing-title {
-        style = display: block;
+      /* GS Definitions Page */
+      
+      .definitions {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        min-height: 100vh;  
+      }
+      
+      .definitions-gs-left {
+        display: flex;
+        flex-direction: row;
+        border-radius: 0 20px 20px 0;
+        border: 3px solid #183D5E;
+        border-left: none;
+        background: #F6F7F8;
+        box-shadow: 4px 4px 12.7px 0 rgba(0, 0, 0, 0.14);
+        width: 73%;
+        overflow: hidden;
+      }
+      
+      .definitions-main-content {
+        display: flex;
+        flex-direction: column;
+        padding: 60px 50px;
+        gap: 22px;
+      }
+      
+      .definitions-sub-content {
+        display: flex;
+        flex-direction: column;
+        padding: 60px 50px;
+        flex: 1;
+        gap: 18px;
+      }
+      
+      .definitions-sub-content-title {
+        display:flex;
+        flex-direction: column;
+      }
+      
+      .definitions-sub-content-itemlist {
+        display:flex;
+        flex-direction: column; 
+        gap: 16px;
+      }
+      
+      .definitions-gs-sub-content-item {
+        display: flex;
+        border-right: 3px solid #164670;
+        padding: 15px 12px;
+        padding-left: 0;
+        justify-content: right;      
+      }
+      
+      .definitions-title {
+        color: #183D5E;
+        text-shadow: 0 4px 4px rgba(0, 0, 0, 0.09);
+        font-family: ChunkFive;
+        font-size: 40px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+      }
+      
+      /* Jacobi Definitions Page */
+      
+      .definitions-jacobi-right {
+        display: flex;
+        flex-direction: row;
+        border-radius: 20px 0px 0px 20px;
+        border: 3px solid #183D5E;
+        border-right: none;
+        background: #F6F7F8;
+        box-shadow: 4px 4px 12.7px 0 rgba(0, 0, 0, 0.14);
+        width: 73%;
+        overflow: hidden;
+      }
+      
+      .definitions-jacobi-sub-content-item {
+        display: flex;
+        border-left: 3px solid #164670;
+        padding: 15px 12px;
+        padding-right: 0;
       }
       
       /* For Placeholders :) */
@@ -139,7 +239,7 @@ ui <- fluidPage(
   # Navbar
   div(class = "custom-navbar",
       a("Landing", href = "#landing", class = "nav-item"),
-      a("Definitions", href = "#definitions", class = "nav-item"),
+      a("Definitions", href = "#definitions-gs", class = "nav-item"),
       a("Foundations", href = "#foundations", class = "nav-item"),
       a("Conditions", href = "#conditions", class = "nav-item"),
       a("Examples", href = "#examples", class = "nav-item"),
@@ -152,20 +252,107 @@ ui <- fluidPage(
       img(src = "assets/landing-photo.png"),
       
       div(class="landing-right",
-        img(src = "assets/title.svg", class="landing-title"),
-        p(style = "text-align: justify;", class = "body-text", 
+        img(src = "assets/title.svg", style = "display: block;"),
+        p(style = "text-align: justify; margin: 0;", class = "body-text", 
           "Lorem ipsum dolor sit amet ", span("highlighted text", class = "highlighted-text"), 
           " adipiscing elit. Ut maximus commodo purus. Nulla eget ligula in tortori aculis 
           fringilla. Vestibulum feugiat dui quis diam convallis mattis. Praesent blandit convallis 
           consectetur. Integer vehicula diam sed ligula ", span("highlighted text", class = 
-          "highlighted-text"), " commodo fringilla.
-        ")
+          "highlighted-text"), " commodo fringilla."
+        )
       )
       
   ),
   
+  # Definitions Page
+  
+  # GS
+  
+  div(id = "definitions-gs", class ="definitions",
+      
+      div(class = "definitions-gs-left",
+        div(style= "width:73%;", class="definitions-main-content",
+          h1(class="definitions-title", "Gauss-Seidel Method"),
+          p(class = "body-text", 
+            "Lorem ipsum dolor sit amet ", span("highlighted text", class = "highlighted-text"), 
+            " adipiscing elit. Ut maximus commodo purus. Nulla eget ligula in tortori aculis 
+          fringilla. Vestibulum feugiat dui quis diam convallis mattis. Praesent blandit convallis 
+          consectetur. Integer vehicula diam sed ligula ", span("highlighted text", class = 
+                                                                  "highlighted-text"), " commodo fringilla."
+          )
+        ),
+        div(style= "flex:1; background:#183D5E;", class="definitions-main-content",
+          h1(style = "color:#F6F7F8;", class="definitions-title", "History"),
+          p(style = "color: #F6F7F8;", class = "body-text", 
+            "Lorem ipsum dolor sit amet ", span("highlighted text", class = "highlighted-text"), 
+            " adipiscing elit. Ut maximus commodo purus. Nulla eget ligula in tortori aculis 
+          fringilla. Vestibulum feugiat dui quis diam convallis mattis. Praesent blandit convallis 
+          consectetur. Integer vehicula diam sed ligula ", span("highlighted text", class = 
+                                                                  "highlighted-text"), " commodo fringilla."
+          )
+        )
+      ),
+      
+      div(class = "definitions-sub-content",
+          div(class="definitions-sub-content-title",
+            img(src = "assets/quote.svg", style = "display: block; width:40px; height:auto"),
+            h1(class="definitions-title", "When do we use it?")
+          ),
+          div(
+            class = "definitions-sub-content-itemlist",
+            lapply(uses_gs, function(item) {
+              div(class = "definitions-gs-sub-content-item",
+                p(style = "text-align: right;", class = "body-text", item)
+              )
+            })
+          )
+      )
+  ),
+  
+  # Jacobi
+  
+  div(id ="definitions-jacobi", class ="definitions",
+      
+      div(class = "definitions-sub-content",
+          div(style="align-items: end;", class="definitions-sub-content-title",
+              img(src = "assets/quote.svg", style = "display: block; width:40px; height:auto"),
+              h1(style="text-align: end;", class="definitions-title", "When do we use it?")
+          ),
+          div(
+            class = "definitions-sub-content-itemlist",
+            lapply(uses_jacobi, function(item) {
+              div(class = "definitions-jacobi-sub-content-item",
+                  p(class = "body-text", item)
+              )
+            })
+          )
+      ),
+      
+      div(class = "definitions-jacobi-right",
+          div(style= "flex:1;", class="definitions-main-content",
+              h1(class="definitions-title", "History"),
+              p(class = "body-text", 
+                "Lorem ipsum dolor sit amet ", span("highlighted text", class = "highlighted-text"), 
+                " adipiscing elit. Ut maximus commodo purus. Nulla eget ligula in tortori aculis 
+          fringilla. Vestibulum feugiat dui quis diam convallis mattis. Praesent blandit convallis 
+          consectetur. Integer vehicula diam sed ligula ", span("highlighted text", class = 
+                                                                  "highlighted-text"), " commodo fringilla."
+              )
+          ),
+          div(style= "width:73%; background:#183D5E;", class="definitions-main-content",
+              h1(style = "color: #F6F7F8", class="definitions-title", "Gauss-Seidel Method"),
+              p(style = "color: #F6F7F8", class = "body-text", 
+                "Lorem ipsum dolor sit amet ", span("highlighted text", class = "highlighted-text"), 
+                " adipiscing elit. Ut maximus commodo purus. Nulla eget ligula in tortori aculis 
+          fringilla. Vestibulum feugiat dui quis diam convallis mattis. Praesent blandit convallis 
+          consectetur. Integer vehicula diam sed ligula ", span("highlighted text", class = 
+                                                                  "highlighted-text"), " commodo fringilla."
+              )
+          )
+      )
+  ),
+  
   # Placeholders
-  div(id = "definitions", class = "section", h2("Definitions Section")),
   div(id = "foundations", class = "section", h2("Foundations Section")),
   div(id = "conditions", class = "section", h2("Conditions Section")),
   div(id = "examples", class = "section", h2("Examples Section")),
